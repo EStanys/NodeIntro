f6e009e11a4d084e814284a0c1826ccfe637a88a
EStanys <edvinasstanys@gmail.com>
2021-05-18 13:55:56 +0300

    created file with node

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const color = "green";
 console.log(`Hello world color is ${color}`);
 
-document.getElementById("id"); // is not defined. Node nera document
+// document.getElementById("id"); // is not defined. Node nera document
 
 // node app.js // paleidziam js faila su node ir gaunam output
 
 // isikeliam failu sistemos moduli
 const fs = require("fs");
 
+// sukurti failus
 fs.writeFile("user1.txt", "username=Bob", (err) => {
   if (err) {
     console.log("err", err);
@@ -15,3 +16,5 @@ fs.writeFile("user1.txt", "username=Bob", (err) => {
     console.log("irasytas failas");
   }
 });
+
+// nuskaityti failus
